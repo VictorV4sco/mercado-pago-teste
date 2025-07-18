ddbe396b9d7abe231c3b208a6c2dc8af7e2271f2
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-07-18 14:32:34 -0300

    My first commit

---
+++ src/main/java/com/mercadopago/MercadoPagoTestApplication.java
@@ -0,0 +1,13 @@
+package com.mercadopago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoPagoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoPagoTestApplication.class, args);
+	}
+
+}
